bd9e6fbda7416502a1ea338e1dd61e0274d25b8a
LukaszFlisikowski <113827755+LukaszFlisikowski@users.noreply.github.com>
2024-11-04 22:02:57 +0100

    Add files via upload

---
+++ can.js
@@ -0,0 +1,221 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+////////////////////////////////////////
+
+ctx.beginPath();
+ctx.moveTo(200,400);
+ctx.lineTo(300,400);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(200,400);
+ctx.lineTo(200,270);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(300,400);
+ctx.lineTo(300,270);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(200,270);
+ctx.lineTo(300,270);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(200,270);
+ctx.lineTo(255,170);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(255,170);
+ctx.lineTo(300,270);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(255,170);
+ctx.lineTo(255,70);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(255,170);
+ctx.lineTo(330,110);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(330,110);
+ctx.lineTo(255,70);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(255,170);
+ctx.lineTo(180,110);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(180,110);
+ctx.lineTo(255,70);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(255,150);
+ctx.lineTo(287,145); ////////bok góra
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(287,145);
+ctx.lineTo(255,130);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(255,130);
+ctx.lineTo(305,130);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(305,130);
+ctx.lineTo(255,110);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(255,110);
+ctx.lineTo(305,95);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(305,95);
+ctx.lineTo(255,85);
+ctx.stroke();
+/// lewy bok 
+ctx.beginPath();
+ctx.moveTo(255,150);
+ctx.lineTo(225,145);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(225,145);
+ctx.lineTo(255,130);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(255,130);
+ctx.lineTo(205,130);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(205,130);
+ctx.lineTo(255,110);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(255,110);
+ctx.lineTo(205,95);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(205,95);
+ctx.lineTo(255,85);
+ctx.stroke();
+// kółka
+ctx.beginPath()
+ctx.arc(250,350,15,0, Math.PI * 2);
+ctx.stroke();
+
+ctx.beginPath()
+ctx.arc(250,310,15,0, Math.PI * 2);
+ctx.stroke();
+// trojkaty
+ctx.beginPath();
+ctx.moveTo(225,400);
+ctx.lineTo(245,420);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(225,400);
+ctx.lineTo(205,420);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(245,420);
+ctx.lineTo(205,420);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(275,400);
+ctx.lineTo(295,420);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(275,400);
+ctx.lineTo(255,420);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(255,420);
+ctx.lineTo(295,420);
+ctx.stroke();
+////////////// Koniec 1 obrazka
+ctx.beginPath();
+ctx.arc(500, 200, 70, 0, Math.PI * 2);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(500, 200, 100, 0, Math.PI * 2);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(750, 200, 90, 0, Math.PI * 2);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(750, 200, 120, 0, Math.PI * 2);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(615, 160, 70, 0, Math.PI * 2);
+ctx.fillStyle = "white";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(615, 240, 70, 0, Math.PI * 2);
+ctx.fillStyle = "white";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(615, 160, 70, 0, Math.PI * 2);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(615, 240, 70, 0, Math.PI * 2);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(615, 240, 69, 0, Math.PI * 2);
+ctx.fillStyle = "white";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(615, 160, 69, 0, Math.PI * 2);
+ctx.fillStyle = "white";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(615, 155, 40, 0, Math.PI * 2);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(615, 245, 40, 0, Math.PI * 2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(575, 245);
+ctx.lineTo(655, 245);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(615, 205);
+ctx.lineTo(615, 285);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(586, 217);
+ctx.lineTo(644, 273);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(643, 216);
+ctx.lineTo(587, 274);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(575, 155);
+ctx.lineTo(655, 155);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(615, 115);
+ctx.lineTo(615, 195);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(586, 127);
+ctx.lineTo(644, 183);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(643, 126);
+ctx.lineTo(587, 184);
+ctx.stroke();
+////// Koniec 2 rysunku
